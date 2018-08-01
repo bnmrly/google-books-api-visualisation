182ea37af3919d5baecb719d0e253139f4759207
Ben Marley <benjaminjohnmarley@gmail.com>
2018-08-01 13:27:24 +0100

    renders book info incl image if exists

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,14 +3,21 @@ import { generate as generateId } from 'shortid';
 import moment from 'moment';
 
 const BookList = ({ books }) => {
-  // console.log(books, '76575');
+  console.log(books, 'this is books');
   return (
     <div className="all-books">
       {books.map(book => {
         const { title, pageCount, publishedDate } = book.volumeInfo;
         return (
           <div className="book-info" key={generateId()}>
-            <h2>{title}</h2>
+            {book.volumeInfo.imageLinks ? (
+              <img src={book.volumeInfo.imageLinks.thumbnail} alt={title} />
+            ) : (
+              <h5>No image available</h5>
+            )}
+            <p>Title: {title}</p>
+            <p>Published date: {moment(publishedDate).format('YYYY')}</p>
+            <p>Pagecount: {pageCount}</p>
           </div>
         );
       })}
