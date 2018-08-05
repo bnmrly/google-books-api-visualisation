f8a23a5e2afe99649194423a0ba04036e03b70d7
Ben Marley <benjaminjohnmarley@gmail.com>
2018-08-05 19:28:43 +0100

    fix image placeholder

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -9,9 +9,16 @@ const BookList = ({ books }) => {
         return (
           <div className="book-info" key={generateId()}>
             {book.volumeInfo.imageLinks ? (
-              <img src={book.volumeInfo.imageLinks.thumbnail} alt={title} />
+              <img
+                src={book.volumeInfo.imageLinks.thumbnail}
+                className="image-thumbnail"
+                alt={title}
+              />
             ) : (
-              <h5>No image available</h5>
+              // <h5>No image available</h5>
+              <div className="image-placeholder">
+                <p className="p-placeholder">No image available</p>
+              </div>
             )}
             <p>Title: {title}</p>
             <p>Published date: {publishedDate.slice(0, 4)}</p>
